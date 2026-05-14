10d9808bd70ca3c3d224cf4f2fb73fe20f1d0483
ftrAL <falgifari36@gmail.com>
2026-05-14 10:42:24 +0800

    Add files via upload

---
+++ index.html/scripts/main.js
@@ -0,0 +1,19 @@
+// togel res nav
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navList = document.querySelector("nav")
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
+// bersihkahkan setelah submit form
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
